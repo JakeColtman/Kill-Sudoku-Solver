72ab086c60a44cd0782c4150cc4ed6c0b40efe87
Jake Coltman <jakecoltman@sky.com>
2015-10-30 07:58:57 +0000

    Added empty implementation of IBlock and a test file

---
+++ Cube/Cube/Block.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cube
+{
+    class Block : IBlock
+    {
+        public Colour get_colour_in_axis(Axes axis)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Cube/CubeTest/Block.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CubeTest
+{
+    [TestClass]
+    public class Block
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
