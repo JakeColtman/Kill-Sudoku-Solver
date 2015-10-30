83014afa73489f4e74e44861867189a4d942b9b8
Jake Coltman <jakecoltman@sky.com>
2015-10-30 07:42:46 +0000

    Added a slice implementation

---
+++ Cube/Cube/Slice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cube
+{
+    class Slice : ISlice
+    {
+        public ISlice rotate(Direction direction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
