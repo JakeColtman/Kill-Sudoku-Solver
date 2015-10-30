08955a2644bd2b805f8528c7c9413e9fc0c47930
Jake Coltman <jakecoltman@sky.com>
2015-10-30 09:15:28 +0000

    Added blank cube implementation

---
+++ Cube/Cube/Cube.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cube
+{
+    class Cube : ICube
+    {
+        public ISlice get_slice(SliceIdentifier identifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool is_complete()
+        {
+            throw new NotImplementedException();
+        }
+
+        public ICube replace_slice(SliceIdentifier identifier, ISlice slice)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
