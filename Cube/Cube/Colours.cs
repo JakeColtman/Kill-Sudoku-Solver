6c5920be9e529ff4ace3ee8cc7c821bdd9443ae3
Jake Coltman <jakecoltman@sky.com>
2015-10-30 07:43:46 +0000

    Added colour enum

---
+++ Cube/Cube/Colours.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cube
+{
+    public enum Colour
+    {
+        Red, 
+        Green, 
+        Orange, 
+        Yellow, 
+        Blue,
+        White
+    }
+}
