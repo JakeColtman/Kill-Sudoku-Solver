79dc5d004a8b12b19fc1222ef244b810bdfe191d
Jake Coltman <jakecoltman@sky.com>
2015-10-30 07:33:38 +0000

    Added enums for axes and face of the cube

---
+++ Cube/Cube/Axes.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cube
+{
+    public enum Axes {  x , y, z }
+
+    public enum Face {  front , back }
+
+}
