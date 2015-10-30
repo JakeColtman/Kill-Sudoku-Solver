3ba9aa18cd973f69dd4ccc1ce0bc1a958fe5eb9b
Jake Coltman <jakecoltman@sky.com>
2015-10-30 07:37:10 +0000

    Added slice identifier to a new file

---
+++ Cube/Cube/SliceIdentifier.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cube
+{
+    public struct SliceIdentifier
+    {
+        public Axes axis;
+        public Face face;
+    }
+}
