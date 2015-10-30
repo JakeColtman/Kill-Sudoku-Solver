4be81025cd39df9f1381d69d137e0417311b949c
Jake Coltman <jakecoltman@sky.com>
2015-10-30 08:24:59 +0000

    Passed the blank colour test

--- Cube/Cube/Block.cs
+++ Cube/Cube/Block.cs
@@ -8,9 +8,52 @@ namespace Cube
 {
     public class Block : IBlock
     {
+
+        IDictionary<Axes, Colour> _colours;
+
+        private Block(IEnumerable<Tuple<Axes, Colour>> colours)
+        {
+            _colours = new Dictionary<Axes, Colour>();
+            foreach(var colour in colours)
+            {
+                _colours.Add(colour.Item1, colour.Item2);
+            }
+        }
+
+        public Block(Axes axis, Colour colour) 
+            : this ( new List<Tuple<Axes, Colour>>()
+            {
+                new Tuple<Axes, Colour>(axis, colour)
+            }
+                )
+        {
+
+        }
+
+        public Block(Axes axis1, Colour colour1, Axes axis2, Colour colour2, Axes axis3, Colour colour3)
+            : this(new List<Tuple<Axes, Colour>>()
+            {
+                new Tuple<Axes, Colour>(axis1, colour1),
+                new Tuple<Axes, Colour>(axis2, colour2),
+                new Tuple<Axes, Colour>(axis3, colour3)
+
+            }
+                )
+        {
+
+        }
+
         public Colour get_colour_in_axis(Axes axis)
         {
-            throw new NotImplementedException();
+            if (!_colours.ContainsKey(axis))
+            {
+                return Colour.Blank;
+            }
+            else
+            {
+                return Colour.Green;
+            }
+            
         }
     }
 }
