7d4c5e0b00a7f4d67dcf6e4c7a5faac4a0dbaf2d
Jake Coltman <jakecoltman@sky.com>
2015-10-30 08:22:09 +0000

    Passed two failing tests for blocks by adding some constructors to block and making it always say its colour is green

--- Cube/CubeTest/Block.cs
+++ Cube/CubeTest/Block.cs
@@ -10,14 +10,28 @@ namespace CubeTest
         [TestMethod]
         public void BlockReturnsAColourInSomeAxis()
         {
-            Cube.IBlock block = new Cube.Block();
+            Cube.IBlock block = new Cube.Block(Cube.Axes.x, Cube.Colour.Blue);
             bool colourFound = false;
             foreach (Cube.Axes axis in Enum.GetValues(typeof(Cube.Axes)))
             {
-                colourFound = colourFound || block.GetType() == typeof(Cube.Colour);
+                colourFound = colourFound || block.get_colour_in_axis(Cube.Axes.x).GetType() == typeof(Cube.Colour);
             }
             Assert.IsTrue(colourFound);
 
         }
+
+        [TestMethod]
+        public void AllAxesReturnAColour()
+        {
+            Cube.IBlock block = new Cube.Block(Cube.Axes.x, Cube.Colour.Blue);
+            bool colourFound = true;
+            foreach (Cube.Axes axis in Enum.GetValues(typeof(Cube.Axes)))
+            {
+                colourFound = colourFound && block.get_colour_in_axis(Cube.Axes.x).GetType() == typeof(Cube.Colour);
+            }
+            Assert.IsTrue(colourFound);
+
+        }
+
     }
 }
