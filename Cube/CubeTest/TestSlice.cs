63f2d9106f6c9e225c37b4b0641760792f8f5f99
Jake Coltman <jakecoltman@sky.com>
2015-10-30 07:48:36 +0000

    Added test setup for as yet empty slice implementation

---
+++ Cube/CubeTest/TestSlice.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CubeTest
+{
+    [TestClass]
+    public class TestSlice
+    {
+
+        Cube.ISlice _slice;
+
+        [TestInitialize]
+        public void setup()
+        {
+            _slice = new Cube.Slice();
+        }
+
+        [TestMethod]
+        public void RotateLeft()
+        {
+            _slice.rotate(Cube.Direction.left);            
+        }
+    }
+}
