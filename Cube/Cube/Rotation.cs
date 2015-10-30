ac0ab806bfc543942df8c436167a2f5ae3ba3381
Jake Coltman <jakecoltman@sky.com>
2015-10-30 09:20:11 +0000

    Added a rotation class

---
+++ Cube/Cube/Rotation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cube
+{
+    class Rotation
+    {
+
+        Axis _axis;
+        Direction _direction;
+
+        public Rotation(Axis axis, Direction direction)
+        {
+            _axis = axis;
+            _direction = direction;
+        }
+
+        public Axis get_axis()
+        {
+            return _axis;
+        }
+
+        public Direction get_direction()
+        {
+            return _direction;
+        }
+
+       
+    }
+}
